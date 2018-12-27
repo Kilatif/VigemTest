b464f832cc94088cd59c8aac26d497b3d9bb1a5b
Vitaly Kolesnikov <kilatif1992@gmail.com>
2018-12-27 18:27:20 +0300

    Changed

--- VigemTest/JoyConsDevice.cs
+++ VigemTest/JoyConsDevice.cs
@@ -3,11 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using HidSharp;
 
 namespace VigemTest
 {
-    class JoyConsDevice
+    enum SixAxisSource
     {
-        
+        Left, Right
+    }
+
+    class JoyConsProDevice
+    {
+        private const int LeftJoyConProductId = 0x2006;
+        private const int RightJoyConProductId = 0x2007;
+
+        private HidDevice _leftJoyCon;
+        private HidDevice _rightJoyCon;
+
+        public SixAxisSource _sixAxisSource = SixAxisSource.Right;
+
+        public void Initialize()
+        {
+            var list = DeviceList.Local;
+
+            var hidDeviceList = list.GetHidDevices().ToArray();
+            hidDeviceList = hidDeviceList.Where(dev => dev.GetManufacturer().Contains("Nintendo")).ToArray();
+        }
     }
 }
